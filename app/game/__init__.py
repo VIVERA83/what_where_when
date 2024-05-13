4c1002ec2186b0f72b90c88a575ccf2416be9262
vivera83 <vivera83@bk.ru>
2024-05-13 23:23:03 +0300

    version_2.1

--- app/game/__init__.py
+++ app/game/__init__.py
@@ -1,11 +1,26 @@
+import json
 import logging
 import re
-from typing import Literal, Any, Union
+from typing import Literal, Any, Union, Callable, Awaitable
 
+from icecream import ic
+
+from game.dc import UserState
 from store.game.cache_manager import CacheAccessor
 from store.game.db_manager import DataBaseManager
 
 EVENT = Literal["text", "callback_data"]
+
+
+def check_cache(func: Callable[["BaseGameAccessor", UserState, Any], Awaitable["UserState"]]):
+    async def wrapper(self: "BaseGameAccessor", tg_user_id: str, *args, **kwargs):
+        user_state_raw = await self.cache.get(tg_user_id)
+        user_state = UserState(**json.loads(user_state_raw))
+        new_user_state = await func(self, user_state, *args, **kwargs)
+        await self.cache.set(new_user_state.tg_user_id, new_user_state.to_string(), 3600)
+        return new_user_state
+
+    return wrapper
 
 
 class BaseGameAccessor:
@@ -18,22 +33,29 @@ class BaseGameAccessor:
         self.db = database
         self.cache = cache
         self.logger = logger
-        self.__text_event_handlers: dict[str, Any] = {}
-        self.__callback_data_handlers: dict[str | re.Pattern, Any] = {}
+        self.__text_event_handlers: dict[
+            str, Callable[["BaseGameAccessor", str, Any], Awaitable["UserState"]]] = {}
+        self.__callback_data_handlers: dict[
+            str | re.Pattern, Callable[["BaseGameAccessor", UserState, Any], Awaitable["UserState"]]] = {}
+        self.init()
 
-    async def event_handler(self, event: EVENT, text_event: str, **kwargs) -> None:
+    def init(self):
+        ...
+
+    async def event_handler(self, event: EVENT, text_event: str, **kwargs) -> bytes:
         """Обработчик событий."""
         self.logger.info(f"event_handler: {event=}, {kwargs=}")
         if event == "callback_data":
-            return await getattr(self, text_event)(**kwargs)
+            return (await getattr(self, text_event)(**kwargs)).to_bytes()
         if event == "text":
-            return self.get_text_event_handler(text_event)(**kwargs)
+            return (await self.get_text_event_handler(text_event)(**kwargs)).to_bytes()
         ValueError("Event must : 'text' or 'callback_data'")
 
-    def get_text_event_handler(self, text):
+    def get_text_event_handler(self, text) -> Callable[["BaseGameAccessor", str, Any], Awaitable["UserState"]]:
         if handler := self.__text_event_handlers.get(text):
             return handler
         raise KeyError(f"Callback data event handler not fount: {text}")
 
-    def add_text_event_handler(self, handler: Any, text: Union[str, re.Pattern]):
+    def add_text_event_handler(self, handler: Callable[["BaseGameAccessor", str, Any], Awaitable["UserState"]],
+                               text: Union[str, re.Pattern]):
         self.__text_event_handlers[text] = handler
--- app/game/start_position.py
+++ app/game/start_position.py
@@ -1,11 +1,15 @@
 from icecream import ic
 
-from . import BaseGameAccessor
+from . import BaseGameAccessor, check_cache
 from .dc import UserState
 
 
 class StartPosition(BaseGameAccessor):
-    async def start(self, tg_user_id: str, *_, **__) -> bytes:
+
+    def init(self):
+        self.add_text_event_handler(self.start, "start")
+
+    async def start(self, tg_user_id: str, *_, **__) -> UserState:
         """Стартовая позиция пользователя в игре.
 
         1. Проверяем зарегистрирован ли пользователь
@@ -16,28 +20,21 @@ class StartPosition(BaseGameAccessor):
         user = await self.db.get_user_by_id(tg_user_id) or await self.db.add_user(tg_user_id=tg_user_id)
         user_state = UserState(
             tg_user_id=user.tg_user_id,
-            position="main",
+            position="start",
             settings={},
         )
         await self.cache.set(user.tg_user_id, user_state.to_string(), 3600)
         self.logger.info(f"start: {user=}")
-        return user_state.to_bytes()
+        return user_state
 
-    async def main(self, tg_user_id: str, *_, **__) -> bytes:
-        """Пользователь в главном меню.
+    @check_cache
+    async def main(self, user_state: UserState, *_, **__) -> UserState:
+        """Пользователь в главном меню."""
+        user_state.position = "main"
+        return user_state
 
-        1. Проверяем позицию пользователя
-            - нет: отправляем на стартовую позицию
-        2. Запоминаем позицию пользователя
-        3. Направляем пользователю позицию в игре
-        """
-
-    async def new_game(self, tg_user_id: str, *_, **__):
-        """Пользователь нажал кнопку "Новая игра".
-
-        1. Проверяем позицию пользователя
-            - не соответствует: отправляем на стартовую позицию
-        2. Запоминаем позицию пользователя
-        3. Направляем пользователю позицию в игре
-        """
-        return UserState(tg_user_id=tg_user_id,position="new_game", settings={}).to_bytes()
+    @check_cache
+    async def new_game(self, user_state: UserState, *_, **__) -> UserState:
+        """Пользователь нажал кнопку "Новая игра"."""
+        user_state.position = "new_game"
+        return user_state
